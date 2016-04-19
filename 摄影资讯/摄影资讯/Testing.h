b316f99d255afdf66abcace0e5d865e17c839d80
G.Z <zerodreaming@sina.com>
2016-04-19 08:57:39 +0800

    第一次 测试

---
+++ 摄影资讯/摄影资讯/Testing.h
@@ -0,0 +1,13 @@
+//
+//  Testing.h
+//  摄影资讯
+//
+//  Created by G.Z on 16/4/19.
+//  Copyright © 2016年 王双月. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Testing : NSObject
+
+@end
---
+++ 摄影资讯/摄影资讯/Testing.m
@@ -0,0 +1,13 @@
+//
+//  Testing.m
+//  摄影资讯
+//
+//  Created by G.Z on 16/4/19.
+//  Copyright © 2016年 王双月. All rights reserved.
+//  testing
+
+#import "Testing.h"
+
+@implementation Testing
+
+@end
